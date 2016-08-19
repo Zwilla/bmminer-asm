761f395fefb175f898bd2d87e63e47f9b9bb781d
Zwilla <info@zwilla.de>
2016-08-19 13:03:43 +0200

    Create driver-btm-c5.h
    this is only that what I found on assembler code
    need some help

---
+++ driver-btm-c5.h
@@ -0,0 +1,140 @@
+//
+//CreatedbyPadillaMiguelon05.08.16.
+//
+
+#ifndefPROJECT_DRIVER_BTM_C5_H
+#definePROJECT_DRIVER_BTM_C5_H
+
+#include"driver-btm-c5.h"
+
+function (get_ticket_mask){
+        
+};
+function (set_ticket_mask){
+
+};
+function (get_job_id){
+
+};
+function (set_job_id){
+
+};
+function (get_job_length){
+
+};
+function (set_job_length){
+
+};
+function (get_block_header_version){
+
+};
+function (set_block_header_version){
+
+};
+function (get_time_stamp){
+
+};
+function (set_time_stamp){
+
+};
+function (get_target_bits){
+
+};
+function (set_target_bits){
+
+};
+function (get_pre_header_hash){
+
+};
+function (set_pre_header_hash){
+
+};
+function (get_coinbase_length_and_nonce2_length){
+
+};
+function (set_coinbase_length_and_nonce2_length){
+
+};
+function (get_work_nonce2){
+
+};
+function (set_work_nonce2){
+
+};
+function (get_merkle_bin_number){
+
+};
+function (set_merkle_bin_number){
+
+};
+function (get_nonce_fifo_interrupt){
+
+};
+function (set_nonce_fifo_interrupt){
+
+};
+function (get_dhash_acc_control){
+
+};
+function (set_dhash_acc_control){
+
+};
+function (get_hash_counting_number){
+
+};
+function (set_hash_counting_number){
+
+};
+function (get_all_temperature){
+
+};
+function (set_PWM_according_to_temperature){
+
+};
+function (set_frequency){
+
+};
+function (set_frequency_with_addr){
+
+};
+function (get_nonce_and_register){
+
+};
+function (read_asic_register){
+
+};
+function (check_asic_reg){
+
+};
+function (check_asic_reg_with_addr){
+
+};
+function (chain_inactive){
+
+};
+function (set_address){
+
+};
+function (software_set_address){
+
+};
+function (set_asic_ticket_mask){
+
+};
+function (set_baud){
+
+};
+function (set_baud_with_addr){
+
+};
+function (open_core){
+
+};
+function (init_uart_baud){
+
+};
+function (check_system_work){
+
+};
+
+#endif//PROJECT_DRIVER_BTM_C5_H
